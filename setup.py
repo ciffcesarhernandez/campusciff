de163ae5364c1c6911655f0b0c3a4da997ecba84
ciffcesarhernandez <cesarhernandez@campusciff.net>
2016-06-13 11:37:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['img'], # this must be the same as the name above
   version = '2.3',
   description = 'my description',
-  author = 'César Hernández',
+  author = 'Cesar Hernandez',
   author_email = 'cesarhernandez@campusciff.net',
   url = 'https://github.com/ciffcesarhernandez/campusciff', # use the URL to the github repo
   download_url = 'https://github.com/ciffcesarhernandez/campusciff/tarball/2.3',
