21ba7d1220f4c1e88b94c52d5b89e35eae5c27e0
ciffcesarhernandez <cesarhernandez@campusciff.net>
2016-06-13 10:27:59 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'img',
+  packages = ['img'], # this must be the same as the name above
+  version = '0.1',
+  description = 'my description',
+  author = 'César Hernández',
+  author_email = 'cesarhernandez@campusciff.net',
+  url = 'https://github.com/ciffcesarhernandez/campusciff', # use the URL to the github repo
+  download_url = 'https://github.com/ciffcesarhernandez/campusciff/tarball/0.1',
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
